136a414afc5674bf8c4684bc0df3db8935aecab2
Carlos Rocabado <crocabado4@gmail.com>
2026-06-16 18:05:33 -0400

    fix: agregar BrowserRouter en main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider }
-from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(
-  document.getElementById(
-    "root"
-  )
+  document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
