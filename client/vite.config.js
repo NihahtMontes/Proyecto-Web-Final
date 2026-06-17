5ee29d1ec57e4d98ba9d0c449addf1df00856f26
Carlos Rocabado <crocabado4@gmail.com>
2026-06-16 23:11:51 -0400

    fix: configurar vite.config.js con proxy hacia backend y soporte tailwindcss

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     port: 5173,
+
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 })
